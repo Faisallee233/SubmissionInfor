eba8e5db01d31622be406345e235faba45803cdb
Faisallee233 <faisallee233@gmail.com>
2025-03-04 11:43:01 +0300

    NewSubInfor

--- SpecialFolder/OriginalFormHandler.php
+++ SpecialFolder/OriginalFormHandler.php
@@ -12,13 +12,17 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
 
    if(!empty($detailsStack[0]) && !empty($detailsStack[1]) && !empty($detailsStack[2])){
       
-    echo "<ul>";
+    if(filter_var($detailsStack[2], FILTER_VALIDATE_EMAIL) && ctype_lower($detailsStack[2]) && preg_match("/^[a-zA-Z-' ]*$/",$detailsStack[1]) && preg_match("/^[a-zA-Z-' ]*$/",$detailsStack[0])){
+        echo "<ul>";
  
     for($i = 0; $i < count($detailsStack); $i++){
         echo "<li>".$detailsStack[$i]."</li>";
     }
 
     echo "<ul>";
+    }else{
+        echo "Invalid input";
+    }
 
    }else{
        echo "Please fill in all fields";
